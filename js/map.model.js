b009de5f63ca23f7caf9f23b88926f290e157a57
Hayden Oster <hayden94@gmail.com>
2017-04-14 14:20:24 -0700

    feat: added Sidebar
    sidebar feature added and map styling and css styling implemented.

---
+++ js/map.model.js
@@ -0,0 +1,13 @@
+var styles = [
+    {
+        "featureType":"all",
+        "elementType":"all",
+        "stylers": [
+            {"invert_lightness":true},
+            {"saturation":10},
+            {"lightness":30},
+            {"gamma":0.5},
+            {"hue":"#435158"}
+        ]
+    }
+]
--- js/map.view.js
+++ js/map.view.js
@@ -1,8 +1,9 @@
 var map;
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map($('#map')[0], {
         center:{lat: 25.7617, lng: -80.1918},
+        styles: styles,
         zoom: 13
     });
 };
