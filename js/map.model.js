2a0b4d99da20b07321c090e4b35ef623868120fc
Hayden Oster <hayden94@gmail.com>
2017-04-15 12:02:11 -0700

    feat: Added markers
    10 markers for Miami's best restaurants have been added with infowindows
    displaying the restaurants title and food category when clicked.

--- js/map.model.js
+++ js/map.model.js
@@ -1,3 +1,11 @@
+var map;
+
+var markers = [];
+
+var mapCenter = {lat: 25.7955, lng: -80.1918};
+
+var iconUrl = 'http://i.imgur.com/yY8lPe2.png';
+
 // map styles that load on initMap
 var styles = [
     {
@@ -12,3 +20,19 @@ var styles = [
         ]
     }
 ];
+
+var locations = [
+    {title: 'Bazaar Mar', location: {lat: 25.761180, lng: -80.193582}, category: 'Seafood'},
+    {title: 'Phuc Yea', location: {lat: 25.840503, lng: -80.184657}, category: 'Vietnamese'},
+    {title: 'Big Easy Whine Bar & Grill', location: {lat: 25.767167, lng: -80.193087}, category: 'South African'},
+    {title: 'Santorini by Georgios', location: {lat: 25.769685, lng: -80.132389}, category: 'Greek'},
+    {title: 'Crust', location: {lat: 25.778406, lng: -80.205942}, category: 'Pizza'},
+    {title: 'Zest Miami', location: {lat: 25.770810, lng: -80.189372}, category: 'Caribbean'},
+    {title: 'Coyo Taco', location: {lat: 25.799690, lng: -80.199316}, category: 'Mexican'},
+    {title: 'Mignonette', location: {lat: 25.793323, lng: -80.190472}, category: 'Seafood'},
+    {title: 'NaiYaRa', location: {lat: 25.794466, lng: -80.144041}, category: 'Thai'},
+    {title: 'Blue Collar Restaurant', location: {lat: 25.837844, lng: -80.184624}, category: 'BBQ'}
+];
+
+
+// https://www.thrillist.com/eat/miami/best-restaurants-miami
--- js/map.view.js
+++ js/map.view.js
@@ -1,17 +1,60 @@
-var map;
-
 function initMap() {
     // create new map with center initializing on Miami
     map = new google.maps.Map($('#map')[0], {
-        center: {lat: 25.7617, lng: -80.1918},
+        center: mapCenter,
         styles: styles,
         zoom: 13
     });
+
+    var largeInfoWindow = new google.maps.InfoWindow();
+    var bounds = new google.maps.LatLngBounds();
 
     // autocomplete functionality binded to the bounds of the map
     var input = $('#zoom-to-area-text')[0];
     var autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.bindTo('bounds', map);
+
+    // Loop through the locations array to retrieve data and display markers
+    for (var i = 0; i < locations.length; i++) {
+        // Get specific location data
+        var position = locations[i].location;
+        var title = locations[i].title;
+        var category = locations[i].category;
+        console.log(position);
+
+        // Create marker per location and put in markers array
+        var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            icon: iconUrl,
+            title: title,
+            category: category,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
+        // Push the marker to our array of markers.
+        markers.push(marker);
+        // Extend bounds of map to position of each marker
+        bounds.extend(marker.position);
+        // Create an on click event to display info window.
+        marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfoWindow);
+        });
+    }
+
+    // This function is called when a marker is clicked, populating it with specific marker data
+    // The function closes the infowindow once the info window is closed
+    function populateInfoWindow(marker, infowindow) {
+        if (infowindow.marker != marker) {
+            infowindow.marker = marker;
+            infowindow.setContent("<div class='infowindow'><h3>" + marker.title + "</h3></br>Category: " + marker.category + "</div>");
+            infowindow.open(map, marker);
+
+            infowinow.addListener('closeclick', function() {
+                infowindow.setMarker = null;
+            });
+        }
+    }
 };
 
 // Get the address from text box and set map to new center once the search button is clicked
