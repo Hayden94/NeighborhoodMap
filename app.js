af54fdce4c57c496ccdbd4429cabe37e2fd8f980
Hayden Oster <hayden94@gmail.com>
2017-05-13 11:14:14 -0700

    chore: Added comments
    Comments added to app.js

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 var express = require("express");
 var app = express();
 const yelp = require('yelp-fusion');
-
+// Yelp API client id and secret stored variables
 const clientId = 'IBd5X_oO6aJyNMPMai8Q3A';
 const clientSecret = 'PFwrZlr2sa5giWHS5btIQSD2aKOZ7n6xOhJ61siLqHZ6vIFMHnzVAulwsyraO1jW';
-
+// port for web server to listen on
 var port = 8080;
 
+// add all directories to web server
 app.use(express.static(__dirname + '/'));
 app.use(express.static(__dirname + '/css'));
 app.use(express.static(__dirname + '/js'));
 app.use(express.static(__dirname + '/img'));
 
+// search request criteria
 const searchRequest = {
-    term: 'Crust',
+    term: 'Bazaar Mar',
     location: 'miami, fl'
 };
 
+// give yelp client API info and issue a response
 yelp.accessToken(clientId, clientSecret).then(response => {
     const client = yelp.client(response.jsonBody.access_token);
 
+    // return first restaurant result based on searchRequest conditions
     client.search(searchRequest).then(response => {
         const firstResult = response.jsonBody.businesses[0];
         const restaurant = JSON.stringify(firstResult, null, 4);
@@ -29,6 +33,7 @@ yelp.accessToken(clientId, clientSecret).then(response => {
     console.log(e);
 });
 
+// return index.html on web server home
 app.get('/', (request, response) => {
   response.sendFile("index.html");
 });
